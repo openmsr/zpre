8c208bb910b495edfe26000cdac42e494eda9317
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-10-06 14:47:51 +0200

    add backend scripts for voxelplot

---
+++ scripts/vp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+if test -f "./h5m_files/zpre.h5m"; then
+  python ./scripts/zpre_geometry_voxelplot.py
+else
+  python ./scripts/step_to_h5m_zpre.py
+  python ./scripts/zpre_geometry_voxelplot.py
+fi
